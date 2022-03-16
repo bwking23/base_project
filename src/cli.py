b6aa706affda43376aeef5c0366d285c855411a7
Barry King <bwking1@gmail.com>
2022-03-16 13:35:10 -0600

    whoo

--- src/cli.py
+++ src/cli.py
@@ -5,13 +5,12 @@ from string_addition import string_addition
 cli_app = typer.Typer()
 
 
-
 @cli_app.command()
-def string_add(value: str):
-    string_value = string_addition(value)
+def string_add(va      lue: str):
+    string_value =    string_addition(value)
     start = "The strings numeric value is "
-    end = typer.style(f"{string_value}", fg=typer.colors.GREEN)
-    typer.echo(start + end)
+    end = typer.style(f"{string_value}", fg=type    r.colors.GREEN)
+    typer.echo(start +  end)
 
 
 if __name__ == "__main__":
