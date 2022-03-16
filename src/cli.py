e81e370bbfa7d582a1b96a3ed3b8112c9e490df1
Barry King <bwking1@gmail.com>
2022-03-16 12:32:50 -0600

    test pre-commit hooks

--- src/cli.py
+++ src/cli.py
@@ -3,14 +3,15 @@ import typer
 from string_addition import string_addition
 
 cli_app = typer.Typer()
+
+
 @cli_app.command()
 def string_add(value: str):
     string_value = string_addition(value)
     start = "The strings numeric value is "
     end = typer.style(f"{string_value}", fg=typer.colors.GREEN)
     typer.echo(start + end)
+
+
 if __name__ == "__main__":
     cli_app()
-
-
-
