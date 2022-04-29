aca13d7c2569f3b9b3d1457accb230029f7a3b27
Barry King <bwking1@gmail.com>
2022-04-29 12:37:36 -0600

    add passing test

--- src/tests/test_string_addition.py
+++ src/tests/test_string_addition.py
@@ -7,6 +7,7 @@ class TestStringAddition:
     def test_addition_success(self):
         assert string_addition("test") == 448
         assert string_addition("long string to add") == 1715
+        assert string_addition("abc") == 24
 
     def test_addition_failure(self):
         with pytest.raises(TypeError) as e_info:
